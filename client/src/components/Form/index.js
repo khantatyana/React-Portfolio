84f3ef7dbe6e39af707250094c7c5851dd7fedde
Tatyana <hanyatan@gmail.com>
2020-10-02 11:04:38 -0400

    deployment

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import emailjs from 'emailjs-com';
-import apiKeys from '../../js/apikeys'
+// import apiKeys from '../../js/apikeys'
 //require('dotenv').config();
 // const dotenv = require('dotenv');
 // import dotenv from 'dotenv';
@@ -8,12 +8,13 @@ import apiKeys from '../../js/apikeys'
 
 export function ContactUs() {
   // const env = dotenv.config().parsed;
-  console.log(process.env.REACT_APP_USER_ID);
+
   function sendEmail(e) {
     e.preventDefault();
     
     emailjs
-      .sendForm("my_amazing_template", apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID)
+      // .sendForm("my_amazing_template", apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID)
+      .sendForm("my_amazing_template", TEMPLATE_ID, e.target, USER_ID)
       .then((result) => {
           console.log(result.text);
           alert("Your message was sent");
