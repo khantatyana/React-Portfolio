279336f0b2e25188e35e3648c1d266932a85f3f1
Tatyana <hanyatan@gmail.com>
2021-02-09 14:24:53 -0500

    updated resume

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -28,7 +28,7 @@ function Contact() {
                         </a>
                     </div>
                     <div className="col-md-6 col-xs">
-                        <a className="social-link" href="https://drive.google.com/file/d/1jqTmwvdHf_96sqm5FXDwVxGtRetVZOAH/view?usp=sharing" target="_blank">
+                        <a className="social-link" href="https://drive.google.com/file/d/1zTJWgWdiiWX9HWGWxgb5I_BV_8g8CjeQ/view?usp=sharing" target="_blank">
                             <i className="far fa-file-word fa-4x fa-spin-hover"></i>Resume
                         </a>
                     </div>
